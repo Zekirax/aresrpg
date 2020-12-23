3a2182c88de0a7e9bc2bbc7394404969723891bd
Alan Dieux <alan.drieux@outlook.fr>
2020-12-23 19:39:05 +0100

    wip: restore item

--- src/inventory.js
+++ src/inventory.js
@@ -2,25 +2,18 @@ import { item_to_slot, empty_slot } from './items.js'
 
 export function reduce_inventory(state, { type, payload }) {
   if (type === 'inventory/window_click') {
-    const { slot, inv_index, out_inv, client } = payload
-    if (!state.cursor_item_selected && out_inv) {
-      restore_items(client, state.world, state.inventory)
-      return state
-    }
-    const _state = {
+    const { inv_index, item, item_save } = payload
+    const newState = {
       ...state,
       inventory: [
         ...state.inventory.slice(0, inv_index),
-        state.cursor_item_selected
-          ? state.cursor_item_selected.item
-          : state.cursor_item_selected,
+        item,
         ...state.inventory.slice(inv_index + 1),
       ],
-      cursor_item_selected: state.inventory[slot]
-        ? { prev_slot: slot, item: state.inventory[slot] }
-        : state.inventory[slot],
+      cursor_item_selected: item_save,
     }
-    return _state
+    // console.log(_state.inventory, _state.cursor_item_selected)
+    return newState
   }
   return state
 }
@@ -28,21 +21,36 @@ export function reduce_inventory(state, { type, payload }) {
 export function listen_inventory({ client, events, dispatch }) {
   client.on('window_click', ({ windowId, slot }) => {
     events.once('state', (state) => {
-      if (windowId !== 0) return
+      if (windowId || slot === -1) return
+      console.log(slot, state.cursor_item_selected)
       const payload = {
-        slot,
         inv_index: slot === -999 ? state.cursor_item_selected.prev_slot : slot,
         out_inv: slot === -999,
-        client,
+        item: state.cursor_item_selected
+          ? state.cursor_item_selected.item
+          : undefined,
+        item_save: state.inventory[slot]
+          ? { prev_slot: slot, item: state.inventory[slot] }
+          : undefined,
       }
+
       dispatch('inventory/window_click', { ...state, ...payload })
+
+      if (payload.out_inv) {
+        events.once('state', (state) => {
+          restore_items(client, state.world, state.inventory)
+        })
+      }
     })
   })
 }
 
 function restore_items(client, world, inventory) {
-  const to_slot = (item) =>
-    item ? item_to_slot(world.items[item.type], item.count) : empty_slot
+  const to_slot = (item) => {
+    if (!item) return empty_slot
+    const { type, count } = item
+    return item_to_slot(world.items[type], count)
+  }
 
   client.write('window_items', {
     windowId: 0,
